91837f8cc4e95086e0efcfc9b16bc21f98fa6bd0
ejeonghun <wjdgns4019@gmail.com>
2024-04-08 13:14:59 +0900

    merge작업

--- lib/test/testscreen_Widget.dart
+++ lib/test/testscreen_Widget.dart
@@ -535,9 +535,6 @@ class _TestscreenWidgetState extends State<TestscreenWidget> {
                               child: FFButtonWidget(
                                 onPressed: () async {
                                   Navigator.of(context)
-                                      .pushNamed('/main1');
-                                },
-                                text: '메인',
                                       .pushNamed('/versusList');
                                 },
                                 text: '대항전 리스트',
@@ -581,6 +578,45 @@ class _TestscreenWidgetState extends State<TestscreenWidget> {
                                       .pushNamed('/versusCreate');
                                 },
                                 text: '대항전 생성',
+                                options: FFButtonOptions(
+                                  height: 40,
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      24, 0, 24, 0),
+                                  iconPadding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 0, 0, 0),
+                                  color: FlutterFlowTheme.of(context).primary,
+                                  textStyle: FlutterFlowTheme.of(context)
+                                      .titleSmall
+                                      .override(
+                                        fontFamily: 'Readex Pro',
+                                        color: Colors.white,
+                                      ),
+                                  elevation: 3,
+                                  borderSide: BorderSide(
+                                    color: Colors.transparent,
+                                    width: 1,
+                                  ),
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 8, 0, 8),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(8, 0, 8, 0),
+                              child: FFButtonWidget(
+                                onPressed: () async {
+                                  Navigator.of(context).pushNamed('/main1');
+                                },
+                                text: '메인페이지',
                                 options: FFButtonOptions(
                                   height: 40,
                                   padding: EdgeInsetsDirectional.fromSTEB(
